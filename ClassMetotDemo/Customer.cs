def837545e17fc3c45553b68c70e2efef0d61488
Nurbanu Kara <nurbanukr98@gmail.com>
2021-02-10 08:07:12 +0300

    10.02.2021

---
+++ ClassMetotDemo/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Customer
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Year { get; set; }
+    }
+}
